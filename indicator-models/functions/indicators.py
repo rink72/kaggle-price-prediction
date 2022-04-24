dba666a064e19557cdd1f32e5a47c40694ea36ea
Sam <beardmans72@gmail.com>
2022-04-25 11:08:38 +1200

    adding indicators

--- indicator-models/functions/indicators.py
+++ indicator-models/functions/indicators.py
@@ -7,10 +7,16 @@ def AddIndicators(data: pd.DataFrame) -> pd.DataFrame:
   updatedData = data.copy()
 
   updatedData = AddAOData(data=updatedData)
+  updatedData = AddAPOData(data=updatedData)
 
   return updatedData
 
 def AddAOData(data: pd.DataFrame) -> pd.DataFrame:
+  updatedData = AddAOIndicator(data=data)
+
+  return updatedData
+
+def AddAOIndicator(data: pd.DataFrame) -> pd.DataFrame:
   updatedData = data.copy()
 
   ao = ta.ao(updatedData['midHigh'], updatedData['midLow'])
@@ -18,5 +24,33 @@ def AddAOData(data: pd.DataFrame) -> pd.DataFrame:
 
   updatedData["bull_signal_ao"] = (updatedData["ao"] > 0) & (updatedData["ao"].shift(1) < 0)
   updatedData["bear_signal_ao"] = (updatedData["ao"] < 0) & (updatedData["ao"].shift(1) > 0)
+
+  return updatedData
+
+def AddAPOData(data: pd.DataFrame):
+  updatedData = data.copy()
+
+  fastRange = range(5, 30, 5)
+  slowRange = range(10, 60, 5)
+
+  paramList = []
+
+  for fr in fastRange:
+      for sr in slowRange:
+        if fr < sr:
+          updatedData = AddAPOIndicator(data=updatedData, apoFast=fr, apoSlow=sr)
+
+  return updatedData
+
+def AddAPOIndicator(data: pd.DataFrame, apoFast: int, apoSlow: int):
+  updatedData = data.copy()
+
+  key = "apo_{0}_{1}".format(apoFast, apoSlow)
+
+  apo = ta.apo(updatedData["midClose"], apoFast, apoSlow)
+  updatedData[key] = apo
+
+  updatedData["bull_signal_{0}".format(key)] = (updatedData[key] > 0) & (updatedData[key].shift(1) < 0)
+  updatedData["bear_signal_{0}".format(key)] = (updatedData[key] < 0) & (updatedData[key].shift(1) > 0)
 
   return updatedData
