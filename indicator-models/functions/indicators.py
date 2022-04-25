a050902d7108bf13d3d50bc3a741979527704927
Sam <beardmans72@gmail.com>
2022-04-25 14:41:30 +1200

    More indicators

--- indicator-models/functions/indicators.py
+++ indicator-models/functions/indicators.py
@@ -8,6 +8,8 @@ def AddIndicators(data: pd.DataFrame) -> pd.DataFrame:
 
   updatedData = AddAOData(data=updatedData)
   updatedData = AddAPOData(data=updatedData)
+  updatedData = AddCCIData(data=updatedData)
+  updatedData = AddCFOData(data=updatedData)
 
   return updatedData
 
@@ -54,3 +56,70 @@ def AddAPOIndicator(data: pd.DataFrame, apoFast: int, apoSlow: int):
   updatedData["bear_signal_{0}".format(key)] = (updatedData[key] < 0) & (updatedData[key].shift(1) > 0)
 
   return updatedData
+
+def AddCCIData(data: pd.DataFrame) -> pd.DataFrame:
+  updatedData = data.copy()
+
+  lengthRange = range(10, 41, 10)
+
+  for lr in lengthRange:
+    updatedData = AddCCIIndicator(data=updatedData, length=lr)
+
+  return updatedData
+
+def AddCCIIndicator(data: pd.DataFrame, length: int) -> pd.DataFrame:
+  updatedData = data.copy()
+
+  key = "cci_{0}".format(length)
+
+  cci = ta.cci(updatedData['midHigh'], updatedData['midLow'], updatedData['midClose'], length)
+  updatedData[key] = cci
+
+  updatedData["bull_signal_{0}".format(key)] = (updatedData[key] >= 100) & (updatedData[key].shift(1) < 100)
+  updatedData["bear_signal_{0}".format(key)] = (updatedData[key] <= -100) & (updatedData[key].shift(1) > -100)
+
+  return updatedData
+
+def AddCFOData(data: pd.DataFrame) -> pd.DataFrame:
+  updatedData = data.copy()
+
+  lengthRange = [7, 14, 21, 28]
+
+  for lr in lengthRange:
+    updatedData = AddCFOIndicator(data=updatedData, length=lr)
+
+  return updatedData
+
+def AddCFOIndicator(data: pd.DataFrame, length: int) -> pd.DataFrame:
+  updatedData = data.copy()
+
+  key = "cfo_{0}".format(length)
+
+  cfo = ta.cfo(updatedData['midClose'], length)
+  updatedData[key] = cfo
+
+  updatedData["bull_signal_{0}".format(key)] = (updatedData[key] >= 0) & (updatedData[key].shift(1) < 0)
+  updatedData["bear_signal_{0}".format(key)] = (updatedData[key] <= 0) & (updatedData[key].shift(1) > 0)
+
+  return updatedData
+
+def AddCGData(data: pd.DataFrame) -> pd.DataFrame:
+  updatedData = data.copy()
+
+  lengthRange = [5, 10, 20, 30, 40]
+
+  for lr in lengthRange:
+    updatedData = AddCGIndicator(data=updatedData, length=lr)
+
+  return updatedData
+
+def AddCGIndicator(data: pd.DataFrame, length: int) -> pd.DataFrame:
+  updatedData = data.copy()
+
+  key = "cg_{0}".format(length)
+
+  cg = ta.cg(updatedData['midClose'], length)
+  updatedData[key] = cg
+
+  updatedData["bull_signal_{0}".format(key)] = (updatedData[key] > updatedData[key].shift(1)) & (updatedData[key].shift(1) < updatedData[key].shift(2))
+  updatedData["bear_signal_{0}".format(key)] = (updatedData[key] < updatedData[key].shift(1)) & (updatedData[key].shift(1) > updatedData[key].shift(2))
